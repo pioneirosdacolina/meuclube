ef9fabccf1682d1a0969c7e664f11777de78b4f5
Mayck Cipriano <mayck.cipriano@gmail.com>
2019-03-03 19:11:41 -0300

    Commit Initial 2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,7 +13,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>{{ env('APP_NAME')}}</title>
 
   <link rel="stylesheet" href="/css/app.css" />
 </head>
