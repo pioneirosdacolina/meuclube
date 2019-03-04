3d2ce6943146e6992df0878da65c9188d77c43cf
Mayck Cipriano <mayck.cipriano@gmail.com>
2019-03-04 18:07:12 -0300

    Controle de acesso.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,6 +25,51 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+
+        Gate::define('isVisitante', function ( $user ){
+            return $user->role === 'visitante';
+        });
+
+        Gate::define('isDesbravador', function ( $user ){
+            return $user->role === 'desbravador';
+        });
+
+        Gate::define('isResponsavel', function ( $user ){
+            return $user->role === 'responsavel';
+        });
+
+        Gate::define('isConselheiro', function ( $user ){
+            return $user->role === 'conselheiro';
+        });
+
+        Gate::define('isInstrutor', function ( $user ){
+            return $user->role === 'instrutor';
+        });
+
+        Gate::define('isAnciao', function ( $user ){
+            return $user->role === 'anciao';
+        });
+
+        Gate::define('isTesoureiro', function ( $user ){
+            return $user->role === 'tesoureiro';
+        });
+
+        Gate::define('isSecretaria', function ( $user ){
+            return $user->role === 'secretaria';
+        });
+
+        Gate::define('isDiretorAssociado', function ( $user ){
+            return $user->role === 'diretor_assoc';
+        });
+
+        Gate::define('isDiretor', function ( $user ){
+            return $user->role === 'diretor';
+        });
+
+        Gate::define('isAdministrador', function ( $user ){
+            return $user->role === 'administrador';
+        });
+
         Passport::routes();
         //
     }
